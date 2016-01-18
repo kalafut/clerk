028a1ccd095d9d91d4f3b5ced60382067d12c564
Jim Kalafut <jim@kalafut.net>
2016-01-17 23:01:51 -0800

    Fix amount test

--- core/amount_test.go
+++ core/amount_test.go
@@ -10,11 +10,11 @@ func TestFormatting(test *testing.T) {
 	is := is.New(test)
 
 	amt := NewAmount("4.25", "$")
-	is.Equal("$4.25", amt.String())
+	is.Equal("$ 4.25", amt.Strings()[0])
 
 	amt = NewAmount("4.259", "$")
-	is.Equal("$4.26", amt.String())
+	is.Equal("$ 4.26", amt.Strings()[0])
 
 	amt = NewAmount("4.25", "AAPL")
-	is.Equal("4.25 AAPL", amt.String())
+	is.Equal("4.25 AAPL", amt.Strings()[0])
 }
