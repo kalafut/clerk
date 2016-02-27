760ed702d232e2ca391867bfd56292418d83efe7
Jim Kalafut <jim@kalafut.net>
2016-02-26 16:57:46 -0800

    More work on Amount

--- amount.py
+++ amount.py
@@ -5,19 +5,32 @@ class Amount:
     """Amounts are full precision (rational) values of one or more commodities, e.g. ($4, 34 AAPL). Though most
     quantities in ledgers deal in a single commodity, is it simpler for any Amount to consist of multiple
     commodities. Some support functions assume a single commodity and will complain otherwise."""
-    def __init__(self, amt, commodity):
-        self.qtys = OrderedDict({ commodity: Decimal(amt) })
+    def __init__(self, param):
+        if isinstance(param, tuple):
+            amt, commodity = param
+            self.qtys = OrderedDict({ commodity: Decimal(amt) })
+        else:
+            assert isinstance(param, dict)
+            self.qtys = param
 
-    def __add__(self, b):
-        ka = set(self.qtys.keys())
-        kb = set(b.qtys.keys())
-        common =  set(ka).union(set(kb))
+    def __add__(self, addend):
+        a,b  = self.qtys, addend.qtys
+        ka = set(a.keys())
+        kb = set(b.keys())
 
-        for k in common:
-            print(k)
+        qtys = {}
 
-        return self
+        for k in ka & kb:
+            qtys[k] = a[k] + b[k]
+
+        qtys.update({k:a[k] for k in (ka - kb)})
+        qtys.update({k:b[k] for k in (kb - ka)})
+
+        return Amount(qtys)
 
     def __str__(self):
         item = next(iter(self.qtys.items()))
         return item[0]+ " " + str(item[1])
+
+    def get(self, cmdty):
+        return Amount((self.qtys[cmdty], cmdty))
--- amount_test.py
+++ amount_test.py
@@ -1,10 +1,28 @@
+import decimal
+
 from unittest import TestCase, main
 from amount import Amount as A
 
 class AmountTestCase(TestCase):
     def test_basic(self):
-        a = A("0.30", "$")
+        a = A(("0.30", "$"))
         self.assertEqual('$ 0.30', str(a))
+
+        a = A({"$": decimal.Decimal(4)})
+        self.assertEqual('$ 4', str(a))
+
+    def test_add(self):
+        a = A(("2.34", "$"))
+        b = A(("5.97", "$"))
+        self.assertEqual("$ 8.31", str(a+b))
+
+        c = A(("9.01", "CAD")) + A(("15.56", "$"))
+        self.assertEqual("CAD 9.01", str(c.get("CAD")))
+        self.assertEqual("$ 15.56", str(c.get("$")))
+
+        d = a + c
+        self.assertEqual("$ 17.90", str(d.get("$")))
+        self.assertEqual("CAD 9.01", str(d.get("CAD")))
 
 if __name__ == '__main__':
     main()
