5b8ccd5217147f7715f113baff28c60e73693001
Jim Kalafut <jim@kalafut.net>
2016-03-30 23:41:38 -0700

    Switch to pytest

--- amount_test.py
+++ amount_test.py
@@ -1,28 +1,24 @@
 import decimal
+import pytest
 
-from unittest import TestCase, main
 from amount import Amount as A
 
-class AmountTestCase(TestCase):
-    def test_basic(self):
-        a = A(("0.30", "$"))
-        self.assertEqual('$ 0.30', str(a))
+def test_basic():
+    a = A(("0.30", "$"))
+    assert '$ 0.30' == str(a)
 
-        a = A({"$": decimal.Decimal(4)})
-        self.assertEqual('$ 4', str(a))
+    a = A({"$": decimal.Decimal(4)})
+    assert '$ 4', str(a)
 
-    def test_add(self):
-        a = A(("2.34", "$"))
-        b = A(("5.97", "$"))
-        self.assertEqual("$ 8.31", str(a+b))
+def test_add():
+    a = A(("2.34", "$"))
+    b = A(("5.97", "$"))
+    assert "$ 8.31" == str(a+b)
 
-        c = A(("9.01", "CAD")) + A(("15.56", "$"))
-        self.assertEqual("CAD 9.01", str(c.get("CAD")))
-        self.assertEqual("$ 15.56", str(c.get("$")))
+    c = A(("9.01", "CAD")) + A(("15.56", "$"))
+    assert "CAD 9.01" == str(c.get("CAD"))
+    assert "$ 15.56" == str(c.get("$"))
 
-        d = a + c
-        self.assertEqual("$ 17.90", str(d.get("$")))
-        self.assertEqual("CAD 9.01", str(d.get("CAD")))
-
-if __name__ == '__main__':
-    main()
+    d = a + c
+    assert "$ 17.90" == str(d.get("$"))
+    assert "CAD 9.01" == str(d.get("CAD"))
