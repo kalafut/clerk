c455149b4aa7eb28aba96cdd496694541b840366
Jim Kalafut <jim@kalafut.net>
2016-03-31 22:17:03 -0700

    Recover some useful old Go files

---
+++ block.go
@@ -0,0 +1,256 @@
+package main
+
+/*
+State machine concept...
+
+States:
+	BeforeBlock
+	LeadingComments
+	Summary
+	Postings
+
+Line classifications:
+	Empty
+	GlobalComment
+	Summary
+	TransactionComment
+	Posting
+*/
+
+/*
+
+const (
+	clsBlank = iota
+	clsComment
+	clsSummary
+	clsPosting
+	clsTxnComment
+	clsInvalid
+)
+
+const (
+	commentChars = ";#|*%"
+	blankChars   = " \t"
+)
+
+var (
+	reBlank      = regexp.MustCompile(`^\s*$`)
+	reComment    = regexp.MustCompile(`^[;#|\*%].*$`)
+	reSummary    = regexp.MustCompile(`^(?P<date>\d{4}/\d\d/\d\d)(?: +(?P<cleared>[!\*]))?(?: +\((?P<code>.*?)\))? +.*$`)
+	rePosting    = regexp.MustCompile(`^\s+[^;#|\*%].*$`)
+	reTxnComment = regexp.MustCompile(`^\s+[;#|\*%].*$`)
+)
+
+// Blocks are a literal encapsulation of a ledger transaction. They
+// are not called transcactions because the actual ledger file strings
+// and comments are preserved. A ledger file is a sequence of blocks.
+//
+// Textually, a block is defined as:
+//    <0+ comment lines>
+//    <0 or 1 summary line: a) left justified  b) starting with a yyyy/mm/dd date
+//    <0+ acccount lines or comments: a) indented at least one space>
+//
+// Whitespace between blocks is ignored.
+
+// Note: values will not have '-', intentionally
+var acctAmtRegex = regexp.MustCompile(`^\s+(.*?\S)(?:\s{2,}.*?([\d,\.]+))?\s*$`)
+
+type Block struct {
+	lines []string
+	date  time.Time
+	valid bool
+}
+
+type ByDate []Block
+
+func (a ByDate) Len() int           { return len(a) }
+func (a ByDate) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByDate) Less(i, j int) bool { return a[i].date.Before(a[j].date) }
+
+// ParseLines turns a chunk of text into a group of Blocks.
+func ParseLines(data io.Reader) []Block {
+	const (
+		stBeforeBlock = iota
+		stLeadingComments
+		stSummary
+		stPostings
+	)
+	var block Block
+	var blocks []Block
+	var state = stBeforeBlock
+
+	scanner := bufio.NewScanner(data)
+
+	for scanner.Scan() {
+		//_ = "breakpoint"
+		line := scanner.Text()
+		switch state {
+		//case stBeforeBlock:
+		//	if len(strings.TrimSpace(line)) > 0 {
+		//		block.lines = append(block.lines, line)
+		//	}
+		}
+
+		if len(strings.TrimSpace(line)) == 0 {
+			if !block.Empty() {
+				blocks = append(blocks, block)
+				block = Block{}
+			}
+		} else {
+			t, err := time.Parse("2006/01/02", line[0:10])
+			if err == nil {
+				// Start a new block
+				if !block.Empty() {
+					blocks = append(blocks, block)
+					block = Block{}
+				}
+				block.date = t
+			}
+			block.lines = append(block.lines, line)
+		}
+	}
+
+	if !block.Empty() {
+		blocks = append(blocks, block)
+	}
+
+	return blocks
+}
+func (b *Block) Empty() bool {
+	return len(b.lines) == 0
+}
+
+func (b Block) Accounts() []string {
+	var ret []string
+	for _, l := range b.lines {
+		m := acctAmtRegex.FindStringSubmatch(l)
+		if len(m) > 0 {
+			ret = append(ret, m[1])
+		}
+	}
+	sort.Strings(ret)
+	return ret
+}
+
+func (b Block) Amounts() []string {
+	var ret []string
+	for _, l := range b.lines {
+		m := acctAmtRegex.FindStringSubmatch(l)
+		if len(m) > 0 {
+			ret = append(ret, m[2])
+		}
+	}
+	sort.Strings(ret)
+	return ret
+}
+
+// IsDupe returns true if other is a likely duplicate based on:
+//   date
+//   affected accounts
+//   amounts
+func (b Block) IsDupe(other Block, tolerance time.Duration) bool {
+	// Check time
+	timeDiff := b.date.Sub(other.date)
+	if timeDiff < 0 {
+		timeDiff = -timeDiff
+	}
+
+	if timeDiff > tolerance {
+		return false
+	}
+
+	// Check affected accounts
+	accts := b.Accounts()
+	acctsOther := other.Accounts()
+
+	if len(accts) != len(acctsOther) {
+		return false
+	}
+
+	for i := range accts {
+		if accts[i] != acctsOther[i] {
+			return false
+		}
+	}
+
+	// Check affected accounts
+	amts := b.Amounts()
+	amtsOther := other.Amounts()
+
+	if len(amts) != len(amtsOther) {
+		return false
+	}
+
+	for i := range amts {
+		if amts[i] != amtsOther[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+// prepareBlock processes []lines data, checking for errors and
+// populating internal fields
+func (b *Block) prepareBlock() {
+	b.valid = true
+}
+
+func classifyLine(line string) (int, map[string]string) {
+	var cls = clsInvalid
+	var data map[string]string
+	var captures []string
+	var matchingRe *regexp.Regexp
+
+	if reBlank.MatchString(line) {
+		cls = clsBlank
+	} else if reComment.MatchString(line) {
+		cls = clsComment
+	} else if rePosting.MatchString(line) {
+		cls = clsPosting
+	} else if reTxnComment.MatchString(line) {
+		cls = clsTxnComment
+	} else if captures = reSummary.FindStringSubmatch(line); len(captures) > 0 {
+		cls = clsSummary
+		matchingRe = reSummary
+	}
+
+	if captures != nil {
+		data = make(map[string]string)
+		for i, key := range matchingRe.SubexpNames() {
+			if i > 0 {
+				data[key] = captures[i]
+			}
+		}
+	}
+
+	return cls, data
+}
+
+// FindDupes returns a list of likely duplicate blocks. Duplicates
+// are block with the same date and transaction structure. The same
+// accounts and amounts must be present in both for it to be dupe.
+func FindDupes(ledger Ledger) {
+	blocks := ledger.blocks
+	for i := range blocks {
+		for j := i + 1; j < len(blocks); j++ {
+			if blocks[i].IsDupe(blocks[j], 0) {
+				fmt.Printf("%v,%v:%v\n", i, j, blocks[i].lines[0])
+			}
+		}
+	}
+}
+
+func NewBlock(t transaction, config AccountConfig) Block {
+	lines := fmt.Sprintf("%s   %s\n", t.date, t.description)
+	lines += fmt.Sprintf("    %s          %s\n", importAcct, t.amount)
+	lines += fmt.Sprintf("    %s", config.TargetAccount)
+
+	blocks := ParseLines(strings.NewReader(lines))
+	if len(blocks) != 1 {
+		log.Fatalf("Expected 1 block, got %+v", blocks)
+	}
+
+	return blocks[0]
+}
+*/
---
+++ block_test.go
@@ -0,0 +1,139 @@
+package main
+
+/*
+
+import (
+	"testing"
+	"time"
+
+	"gopkg.in/tylerb/is.v1"
+)
+
+const day = 24 * time.Hour
+
+func TestIsDupe(t *testing.T) {
+	is := is.New(t)
+	now := time.Now()
+
+	b_base := Block{
+		date: now,
+		lines: []string{
+			"2015/10/04 Doctor Roberts",
+			"    Everyday Expenses:Medical        $10.00",
+			"    Assets:Bank:Chase Checking",
+		},
+	}
+
+	// Test date
+	b_test := cloneBlock(b_base)
+	is.True(b_base.IsDupe(b_test, 0*day))
+
+	b_test.date = now.Add(1 * day)
+	is.True(b_base.IsDupe(b_test, 1*day))
+
+	b_test.date = now.Add(2 * day)
+	is.False(b_base.IsDupe(b_test, 1*day))
+	is.True(b_base.IsDupe(b_test, 2*day))
+
+	b_test.date = now.Add(-1 * day)
+	is.True(b_base.IsDupe(b_test, 1*day))
+
+	b_test.date = now.Add(-2 * day)
+	is.False(b_base.IsDupe(b_test, 1*day))
+	is.True(b_base.IsDupe(b_test, 2*day))
+
+	// Test account change differences
+	b_test = cloneBlock(b_base)
+	is.True(b_base.IsDupe(b_test, 0*day))
+
+	b_test.lines = append(b_test.lines, "    Assets:Bank:Chase Checking")
+	is.False(b_base.IsDupe(b_test, 0*day))
+
+	b_test = cloneBlock(b_base)
+	b_test.lines[2] = "    Assets:Bank:Chase Checking"
+	is.True(b_base.IsDupe(b_test, 0*day))
+	b_test.lines[2] = "       Assets:Bank:Chase Checking   "
+	is.True(b_base.IsDupe(b_test, 0*day))
+
+	b_test.lines[2] = "    Assets:Bank:Zhase Checking"
+	is.False(b_base.IsDupe(b_test, 0*day))
+
+	// Test amount change differences
+	b_test = cloneBlock(b_base)
+	is.True(b_base.IsDupe(b_test, 0*day))
+
+	b_test.lines[1] = "    Everyday Expenses:Medical        $-10.00"
+	is.True(b_base.IsDupe(b_test, 0*day))
+
+	b_test.lines[1] = "    Everyday Expenses:Medical        $10.01"
+	is.False(b_base.IsDupe(b_test, 0*day))
+
+	b_test.lines[2] = "    Assets:Bank:Chase Checking       $10.00"
+	is.False(b_base.IsDupe(b_test, 0*day))
+
+	b_test = cloneBlock(b_base)
+	b_test.lines[1], b_test.lines[2] = b_test.lines[2], b_test.lines[1]
+	is.True(b_base.IsDupe(b_test, 0*day))
+}
+
+func TestClassifyLine(t *testing.T) {
+	type ss map[string]string
+	is := is.New(t)
+
+	tests := []struct {
+		line  string
+		class int
+		data  ss
+	}{
+		{"", clsBlank, nil},
+		{";Comment", clsComment, nil},
+		{"2015/03/25 Simple summary", clsSummary, ss{"date": "2015/03/25", "cleared": "", "code": ""}},
+		{"2015/03/25Simple summary", clsInvalid, nil},
+		{"2015/03/25 ! Simple summary", clsSummary, ss{"date": "2015/03/25", "cleared": "!", "code": ""}},
+		{"2015/03/25 * Simple summary", clsSummary, ss{"date": "2015/03/25", "cleared": "*", "code": ""}},
+		{"2015/03/25 * (523) Simple summary", clsSummary, ss{"date": "2015/03/25", "cleared": "*", "code": "523"}},
+		{"2015/03/25   !   (1a2)  Simple summary", clsSummary, ss{"date": "2015/03/25", "cleared": "!", "code": "1a2"}},
+		{" a posting", clsPosting, nil},
+		{" #a comment", clsTxnComment, nil},
+	}
+
+	for _, t := range tests {
+		//println(t.line)
+		cls, data := classifyLine(t.line)
+		is.Equal(cls, t.class)
+		is.Equal(len(data), len(t.data))
+		for k, _ := range data {
+			is.Equal(data[k], t.data[k])
+		}
+	}
+}
+
+func TestNewBlock(t *testing.T) {
+	is := is.New(t)
+
+	acctCfg := AccountConfig{
+		TargetAccount: "Assets:Bank:Chase Checking",
+	}
+	transaction := transaction{
+		date:        "2006/12/01",
+		description: "Test description",
+		amount:      "$65.00",
+	}
+
+	block := NewBlock(transaction, acctCfg)
+
+	is.Equal("2006/12/01   Test description", block.lines[0])
+	is.Equal("    __Uncategorized__          $65.00", block.lines[1])
+	is.Equal("    Assets:Bank:Chase Checking", block.lines[2])
+}
+
+func cloneBlock(orig Block) Block {
+	b := Block{
+		date:  orig.date,
+		lines: make([]string, len(orig.lines)),
+	}
+	copy(b.lines, orig.lines)
+
+	return b
+}
+*/
